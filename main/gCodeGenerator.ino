4214b2487bb37af3f1b174e8de3095cc96fd4da3
zitske <eduardomontzitske@gmail.com>
2022-12-26 14:44:00 -0300

    organization

--- main/gCodeGenerator.ino
+++ main/gCodeGenerator.ino
@@ -1,16 +1,28 @@
-#include <iostream>
-#include <string>
 
-using namespace std;
 
-string goHome(){
-    return "G00 X0.0000 Y0.0000";
+void setup(){
+    Serial.begin(115200);
 }
 
+void loop()
+{
+    int lenght=50;
+    double middle=111.0;
+    int tm=5;
+    bool cc = false;
+    
+    linear(lenght,middle,tm);
+    circular(lenght,middle,tm,cc);
+
+}
+
+String goHome(){
+    return "G00 X0.0000 Y0.0000";
+}
 void linear(int lenght,double middle,int times){
     bool side = true;
     Serial.println(goHome());
-    Serial.println("G00 X"+middle+lenght/2+"Y"+middle);
+    Serial.println("G00 X"+String(middle+lenght/2)+"Y"+String(middle));
     
     for(int i=0;i<times;i++){
         if(side){
@@ -23,30 +35,17 @@ void linear(int lenght,double middle,int times){
     }
     Serial.println(goHome());
 }
-
 void circular(int radius,double middle,int times,bool cc){
     Serial.println(goHome());
-    Serial.println("G00 X"+middle+radius+"Y"+middle);
+    Serial.println("G00 X"+String(middle)+radius+"Y"+String(middle));
     
     for(int i=0;i<times;i++){
         if(cc){
-            Serial.println("G2 X"+radius+"I"+radius+"J0");
+            Serial.println("G2 X"+String(radius)+"I"+String(radius)+"J0");
         }else{
-            Serial.println("G3 X"+radius+"I"+radius*(-1)+"J0");
+            Serial.println("G3 X"+String(radius)+"I"+String(radius*(-1))+"J0");
         }
     }
     Serial.println(goHome());
     
-}
-
-int main()
-{
-    int lenght=50;
-    double middle=111.0;
-    int tm=5;
-    bool cc = false;
-    
-    linear(lenght,middle,tm);
-    circular(lenght,middle,tm,cc);
-    return 0;
 }
